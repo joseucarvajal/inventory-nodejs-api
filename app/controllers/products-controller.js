e62db4fdcf0c4744b4100c4fb210052931fb4aa5
joseucarvajal <joseucarvajal@gmail.com>
2020-05-19 14:28:13 -0500

    Backend data normalized, CORS rules added

--- app/controllers/products-controller.js
+++ app/controllers/products-controller.js
@@ -1,10 +1,15 @@
 import dbQuery from "../db/query";
 
-import { errorMessage, successMessage, status  } from "../helpers/http-status-helper";
+import { errorMessage, successMessage, status } from "../helpers/http-status-helper";
 
 export const getProductsByCategory = async (request, response) => {
 
-    const getProductsQuery = 'SELECT * from products';
+    const getProductsQuery = `
+        SELECT 
+            c.id as categoryid, c.name as categoryname, p.id, p.name, p.price 
+        FROM 
+            category c INNER JOIN products p ON p.category = c.id
+    `;
 
     const { rows } = await dbQuery.query(getProductsQuery);
 
@@ -13,6 +18,35 @@ export const getProductsByCategory = async (request, response) => {
         return response.status(status.bad).errorMessage();
     }
 
-    successMessage.data = rows;
-    return response.status(status.success).send(successMessage) ;
+    const hashCategories = {};
+    for (let row of rows) {
+        if (!hashCategories[row.categoryid]) {
+            hashCategories[row.categoryid] = {
+                id: row.categoryid,
+                name: row.categoryname,
+                products: [
+                    {
+                        id: row.id,
+                        name: row.name,
+                        price: row.price
+                    }
+                ]
+            }
+        }
+        else {
+            hashCategories[row.categoryid].products.push({
+                id: row.id,
+                name: row.name,
+                price: row.price                
+            });
+        }
+    }
+    
+    const categoriesData = [];
+    for(let category in hashCategories){
+        categoriesData.push(hashCategories[category]);        
+    }    
+    
+    successMessage.data = categoriesData;
+    return response.status(status.success).send(successMessage);
 }
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 
+import cors from 'cors';
+
 import calculatorRoutes from "./app/routes/calculator-routes";
 import productsRoutes from "./app/routes/product-routes";
 
 const app = express();
+
+// Add middleware for parsing URL encoded bodies (which are usually sent by browser)
+app.use(cors({origin:'http://localhost:3001'}));
 
 // Add middleware for parsing JSON and urlencoded data and populating `req.body`
 app.use(express.urlencoded({ extended: false }));
